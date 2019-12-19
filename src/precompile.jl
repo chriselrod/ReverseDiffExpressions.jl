252fd5d51e9172717f00f560d16993d8656b32d0
Chris Elrod <elrodc@gmail.com>
2019-12-19 03:33:36 -0500

    Updated manifest and precompile.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,3 +1,6 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
+    precompile(Tuple{typeof(Base.unsafe_convert),Type{Ref{typeof(ReverseDiffExpressions.diagonal_diff_rule!)}},Base.RefValue{typeof(ReverseDiffExpressions.diagonal_diff_rule!)}})
+    precompile(Tuple{typeof(Base.unsafe_convert),Type{Ref{typeof(ReverseDiffExpressions.getindex_diff_rule!)}},Base.RefValue{typeof(ReverseDiffExpressions.getindex_diff_rule!)}})
+    precompile(Tuple{typeof(Base.unsafe_convert),Type{Ref{typeof(ReverseDiffExpressions.mul_diff_rule!)}},Base.RefValue{typeof(ReverseDiffExpressions.mul_diff_rule!)}})
 end
