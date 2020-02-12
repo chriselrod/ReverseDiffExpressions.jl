0e1d7b360e74a41b70e4410d0325fa58fe4e6175
Chris Elrod <elrodc@gmail.com>
2020-02-12 00:21:29 -0500

    Added start to testing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,18 @@
-using ReverseDiffExpressions
+using ReverseDiffExpressions, LoopVectorization
 using Test
 
 @testset "ReverseDiffExpressions.jl" begin
 
-    
+    AmulBq = :(for m ∈ 1:M, n ∈ 1:N
+                C[m,n] = zero(eltype(B))
+                for k ∈ 1:K
+                C[m,n] += A[m,k] * B[k,n]
+                end
+                end)
+    lsAmulB = LoopVectorization.LoopSet(AmulBq);
+
+    ∂ls = ReverseDiffExpressions.LoopSetDerivatives.∂LoopSet(lsAmulB)
+    ∂ls.fls
+    ∂ls.rls
     # Write your own tests here.
 end
